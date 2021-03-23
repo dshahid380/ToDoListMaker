488bf23f67e59be13c1a07ceae626112e8e82444
mdshahid380 <shahid.shahid@telus.com>
2021-03-23 11:05:47 +0530

    Initialized Spring Backend.

---
+++ backend/src/test/java/com/restful/services/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restful.services.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
